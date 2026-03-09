b4d1f6ef74b0e74e5fd58f7631a426642c6de648
KeyMarmot <127695565+KeyMarmot@users.noreply.github.com>
2026-03-09 09:20:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-
 console.log('Script Starting...')
 const canvas = document.getElementById('canvas');
 const Changer = document.getElementById('AttChange');
 const ctx = canvas.getContext('2d');
-const hAccel = document.getElementById('hAccel');
-const vAccel = document.getElementById('vAccel');
-const hspeed = document.getElementById('hSpeed');
-const vspeed = document.getElementById('vSpeed');
 const color = document.getElementById('color');
 const size = document.getElementById('size');
 const startPosx = document.getElementById('startingPosx');
-const startPosy = document.getElementById('startingPosy')
+const startPosy = document.getElementById('startingPosy');
+const speed = document.getElementById('Speed');
+const direction = document.getElementById("StartingDirection");
+let dx = 0;
+let dy=0;
 
 document.addEventListener('keydown', handleKeyPress);
 document.addEventListener('keydown', changeAttributes)
@@ -30,89 +29,63 @@ function drawCircle(x, y, radius, fill, startAngle=0, endAngle=360){
     ctx.fill();
 }
 
-//circle definitions
 let circle = {
+    speed: 2,
     x: 32, 
     y: 32,
     radius: 16,
-    haccelerate: .25,
-    vaccelerate: 1,
-    hspeed: 1,
-    vspeed: 1,
-    fill: 'green'
+    fill: 'green',
+    direction: 7*Math.PI/4
 }
 
-//Add lookahead
-//movement code & instructions
 function moveWithBounce(circle){
-    circle.hspeed += circle.haccelerate;
-    circle.vspeed += circle.vaccelerate;
-    circle.x += circle.hspeed;
-    circle.y += circle.vspeed;
-    console.log(circle.vspeed)
+    //console.log(dy)
+    dx = circle.speed*Math.cos(circle.direction);
+    dy = circle.speed*Math.sin(circle.direction);
+    circle.x += dx;
+    circle.y += dy;
 
-    //speed cap
-    if (circle.hspeed >= 5 || circle.hspeed <= -5){
-        circle.haccelerate = 0;
-    }
-    //bouncing rules
-    if (circle.x-circle.radius < 0 || circle.x+circle.radius > canvas.width){
-        circle.hspeed *= -1;
-    }
-
-    if (circle.x-circle.radius < 0 || circle.x+circle.radius > canvas.width){
-        circle.haccelerate *= -1;
+    //bounce rules
+    if(circle.x-circle.radius<0 || circle.x-circle.radius > canvas.width){
+        dx *= -1;
     }
 
     if (circle.y-circle.radius < 0 || circle.y+circle.radius > canvas.height){
-        circle.vspeed *= -.99;
-        circle.y = canvas.height-circle.radius
-    }
-
-    if (circle.vspeed < .45 && circle.vspeed > -.45){
-        circle.vspeed = 0;
-        circle.vaccelerate = 0;
+        dy *= -1;
     }
 }
-
-
 function changeAttributes(circle){
-    circle.vaccelerate = Number(vAccel.value);
-    if (vAccel.value == ""){
-        circle.vaccelerate = 1
-    }
-    circle.haccelerate = Number(hAccel.value);
-    if (hAccel.value == ""){
-        circle.haccelerate = .25
-    }
-    circle.vspeed = Number(vspeed.value);
-    if (vspeed.value == ""){
-        circle.vspeed = 1
-    }
-    circle.hspeed = Number(hspeed.value);
-    if (hspeed.value == ""){
-        circle.hspeed = 1
-    }
     circle.radius = Number(size.value);
     if (size.value == ""){
         circle.radius = 16
     }
+
     circle.x = Number(startPosx.value);
     if (startPosx.value == ""){
         circle.x = 32
     }
+
     circle.y = Number(startPosy.value);
     if (startPosy.value == ""){
         circle.y = 32
     }
+
     circle.fill = color.value;
     if (color.value == ""){
         circle.fill = 'green'
     }
+
+    circle.direction = direction.value;
+    if (direction.value == ''){
+        circle.direction = 7*Math.PI/4
+    }
+
+    circle.speed = speed.value;
+    if (speed.value ==''){
+        circle.speed = 2
+    }
     console.log(circle)
 }
-
-
 
 function drawLoop(){
     //clear canvas
@@ -128,8 +101,3 @@ function drawLoop(){
 Changer.onclick = () => changeAttributes(circle);
 
 drawLoop();
-
-
-
-
-
